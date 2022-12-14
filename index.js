f96334551f6e65f00fee3160a01051670cdf62cb
tomerdu <YOUR-EMAIL>
2022-12-14 19:01:51 +0200

    add user successfully

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const {app} = require("./server/routers/mainRoute");
 const port = process.env.PORT || 4020;
-// const mongoose = require("mongoose");
-// mongoose.set('strictQuery', true);
+const mongoose = require("mongoose");
+const MongoStorage = require("./server/data/mongoStorage");
+mongoose.set('strictQuery', true);
+const DB = new MongoStorage();
+const path = require("path");
+
+require('dotenv').config();
 
 app.listen(port, () => console.log(`Express server is running on port ${port}`));
--- server/routers/mainRoute.js
+++ server/routers/mainRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
 
 const signUp = require("./signUpRoute");
+
+app.use(bodyParser.json());
 
 app.use('/signUp', signUp.signupRoute);
 
