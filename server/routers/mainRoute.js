0d680614e39b1ef299e502fbe488743cb74c6f02
tomerdu <YOUR-EMAIL>
2022-12-15 09:32:17 +0200

    add user to user schema successfully

--- server/routers/mainRoute.js
+++ server/routers/mainRoute.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 
 const signUp = require("./signUpRoute");
+const login = require('./loginRoute');
+
 
 app.use('/signUp', signUp.signupRoute);
+app.use('/login', login.loginRouter);
 
 
 module.exports = {app}
