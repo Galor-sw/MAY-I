f17e59cc40cb493837d334cb7a9f9e10315fb96c
tomerdu <YOUR-EMAIL>
2022-12-15 13:03:36 +0200

    login - req is not defined

---
+++ server/controllers/loginController.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const dbRepository = require('../services/dbRepository');
+
+
+const handleLogin = async (req, res, next) => {
+    const Euser = req.body
+    const userEmail = Euser.email.toLowerCase();
+    const userPassword = req.body.password;
+    const user = await dbRepository.getUserByEmail(userEmail)
+    if (!user) {
+        throw new Error("user doesn't exist")
+    }
+    if (!await bcrypt.compare(userPassword, user.password)) {
+        throw new Error("incorrect password")
+    }
+    res.status(200);
+    res.redirect("/homePage")
+}
+
+module.exports = {handleLogin};
---
+++ server/routers/loginRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const loginRouter = new express.Router();
+const login_controller = require('../controllers/loginController');
+
+loginRouter.use(express.json());
+
+loginRouter.post('/', login_controller.handleLogin);
+
+module.exports = {loginRouter};
